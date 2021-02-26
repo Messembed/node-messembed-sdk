d3d63ac9a53b5ad4a20a3f965ed22400c5f2610f
Edgar <starguitar307@gmail.com>
2021-02-26 17:42:39 +0200

    build: esModuleInterop true

--- dist/messembed-sdk.js
+++ dist/messembed-sdk.js
@@ -1,4 +1,23 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __rest = (this && this.__rest) || function (s, e) {
     var t = {};
     for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
@@ -10,11 +29,14 @@ var __rest = (this && this.__rest) || function (s, e) {
         }
     return t;
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.MessembedSDK = void 0;
-const axios_1 = require("axios");
-const _ = require("lodash");
-const socket_io_client_1 = require("socket.io-client");
+const axios_1 = __importDefault(require("axios"));
+const _ = __importStar(require("lodash"));
+const socket_io_client_1 = __importDefault(require("socket.io-client"));
 const events_1 = require("events");
 const DATE_FIELDS = ['createdAt', 'updatedAt', 'deletedAt'];
 const MESSAGE_DATE_FIELDS = [...DATE_FIELDS, 'readAt'];
