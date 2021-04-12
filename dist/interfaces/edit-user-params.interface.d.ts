c801f328800f72d23ba0d45c9dbf8865fac9ca6c
Edgar Pogosyan <edgar.pogosyan.a@gmail.com>
2021-04-12 10:07:46 +0200

    chore: build and bump version to 0.2.2

--- dist/interfaces/edit-user-params.interface.d.ts
+++ dist/interfaces/edit-user-params.interface.d.ts
@@ -2,4 +2,6 @@ export interface EditUserParams {
     userId: string;
     externalMetadata?: Record<string, unknown>;
     privateExternalMetadata?: Record<string, unknown>;
+    blockStatus?: 'CANT_SEND_AND_RECEIVE_NEW_MESSAGES' | null;
+    removeBlockStatus?: boolean;
 }
