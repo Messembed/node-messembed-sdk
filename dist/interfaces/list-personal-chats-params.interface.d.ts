4808343d2db2ea462e8e54443067bda7ce793d84
Edgar Pogosyan <edgar.pogosyan.a@gmail.com>
2021-06-10 13:28:24 +0200

    feat: add includeInactive to ListPersonalChatsParams

--- dist/interfaces/list-personal-chats-params.interface.d.ts
+++ dist/interfaces/list-personal-chats-params.interface.d.ts
@@ -2,4 +2,5 @@ export declare class ListPersonalChatsParams {
     query?: string;
     sort?: 'NEWER_FIRST' | 'UNREAD_FIRST';
     externalMetadata?: Record<string, unknown>;
+    includeInactive?: boolean;
 }
--- src/interfaces/list-personal-chats-params.interface.ts
+++ src/interfaces/list-personal-chats-params.interface.ts
@@ -4,5 +4,13 @@ export class ListPersonalChatsParams {
    */
   query?: string;
   sort?: 'NEWER_FIRST' | 'UNREAD_FIRST';
-  externalMetadata?: Record<string, unknown>
+  externalMetadata?: Record<string, unknown>;
+
+  /**
+   * Чтобы в результате были также неактивные чаты (с `active: false`)
+   * поставьте этот параметр true.
+   *
+   * @default false
+   */
+  includeInactive?: boolean;
 }
